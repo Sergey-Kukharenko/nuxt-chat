39cf3085b30bc2e2ced390251903ed99ce1b7523
Sergey-Kukharenko <Kuharenko@inventive.ru>
2022-03-14 19:21:47 +0300

    initial

--- plugins/socket.js
+++ plugins/socket.js
@@ -1,20 +1,11 @@
 import Vue from 'vue'
 import VueSocketIO from 'vue-socket.io'
-import io from 'socket.io-client';
-
-const socketInstance = io('/', {
-  rejectUnauthorized: false,
-  reconnection: true,
-  reconnectionDelay: 1000,
-  reconnectionDelayMax : 5000,
-  reconnectionAttempts: 15,
-});
 
 export default function({ store }) {
   Vue.use(
     new VueSocketIO({
       debug: false,
-      connection: socketInstance,
+      connection: 'http://localhost:3000',
       vuex: {
         store,
         actionPrefix: 'SOCKET_',
