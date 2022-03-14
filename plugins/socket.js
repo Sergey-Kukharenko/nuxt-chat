cbe605dc9862abfa36dd98b78a06b85e136802e3
Sergey-Kukharenko <Kuharenko@inventive.ru>
2022-03-14 19:35:47 +0300

    initial

--- plugins/socket.js
+++ plugins/socket.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import VueSocketIO from 'vue-socket.io'
+const options = { path: '/socket.io/' };
 
 export default function({ store }) {
   Vue.use(
     new VueSocketIO({
       debug: false,
       connection: 'http://localhost:3000',
+      options,
       vuex: {
         store,
         actionPrefix: 'SOCKET_',
