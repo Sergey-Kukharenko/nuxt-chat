e6aa90d376107fac020ce01aa6f2c92b2f249377
Sergey-Kukharenko <Kuharenko@inventive.ru>
2022-03-15 10:38:12 +0300

    initial

--- plugins/socket.js
+++ plugins/socket.js
@@ -5,7 +5,7 @@ export default function({ store }) {
   Vue.use(
     new VueSocketIO({
       debug: false,
-      connection: 'https://stoic-stonebraker-26d72b.netlify.app',
+      connection: 'https://stoic-stonebraker-26d72b.netlify.app' || 'http://localhost:3000',
       vuex: {
         store,
         actionPrefix: 'SOCKET_',
--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 const server = require('http').createServer(app)
-const io = require('socket.io')(server)
-const io = require('socket.io')(http,  { cors: { origin: '*' } });
+// const io = require('socket.io')(server)
+const io = require('socket.io')(server,  { cors: { origin: '*' } });
 const users = require('./users')()
 
 const m = (name, text, id) => ({name, text, id})
